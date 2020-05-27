7ee06f6f54de60c31d18026b24fff028fd5d7afa
Takenori Morio <morikapusan@eleken.jp>
2020-05-27 11:56:22 +0900

    Del:未使用スクリプトの削除

--- script.js
+++
@@ -1,9 +0,0 @@
-chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
-	if (request == "Action") {
-		filterProduct();
-	}
-});
-
-function filterProduct() {
-	location.href = location.href + "&emi=AN1VRQENFRJN5";
-}
